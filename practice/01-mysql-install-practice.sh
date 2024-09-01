84cd5e1efe2062ed36b84b1decb0be1ececee565
deepakgajula <gajuladeepak17@gmail.com>
2024-09-01 17:25:18 +0530

    Shell

--- practice/01-mysql-install-practice.sh
+++ practice/01-mysql-install-practice.sh
@@ -9,3 +9,18 @@ then
 
 dnf list installed $SOFTWARE
 
+if [ $? -ne 0 ]
+then
+    echo "Need to install $SOFTWARE.. Not yet installed"
+    dnf install $SOFTWARE -y
+    if [ $? -ne 0 ]
+    then
+        echo "Sorry Something wwnt wrong..."
+        exit 1
+    else
+        echo "$SOFTWARE instllation is successfull"
+    fi
+else
+    echo "Already Installed..No need to install again"
+
+fi
