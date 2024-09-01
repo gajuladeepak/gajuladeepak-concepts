ea81d7ca2877afa65c41d49c44d165dbaf1b3bd1
deepakgajula <gajuladeepak17@gmail.com>
2024-09-01 16:57:50 +0530

    Shell

---
+++ 01-mysql-install-practice.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+USERID=$(id -u)
+SOFTWARE=$1
+
+if [ $USERID -ne 0 ]
+then
+    echo "Root access is requried"
+    exit 1
+
+dnf list installed $SOFTWARE
+
+if [ $? -ne 0 ]
+then
+    echo "Need to install $SOFTWARE.. Not yet installed"
+    dnf install $SOFTWARE -y
+    if [ $? -ne 0 ]
+    then
+        echo "Sorry Something wwnt wrong..."
+        exit 1
+    else
+        echo "$SOFTWARE instllation is successfull"
+else
+    echo "Already Installed..No need to install again"
