36cbc9af1ba04bff202e23fc315e4a9b3f5868d9
deepakgajula <gajuladeepak17@gmail.com>
2024-09-01 18:17:06 +0530

    Shell

---
+++ practice/02-functions.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+USERID=$(id -u)
+SOFTWARE=$1
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Root access is requried"
+        exit 1
+    fi
+    
+}
+
+INSTALLATION_CHECK(){
+    if [ $1 -ne 0 ]
+    then
+        echo "Sorry Something wwnt wrong..."
+        exit 1
+    else
+        echo "$2 instllation is successfull"
+    fi
+
+}
+CHECK_ROOT
+
+
+if [ -z $SOFTWARE ]
+then    
+    echo "Please provide input to the command"
+    exit 1
+fi
+
+
+
+dnf list installed $SOFTWARE
+
+if [ $? -ne 0 ]
+then
+    echo "Need to install $SOFTWARE.. Not yet installed"
+    dnf install $SOFTWARE -y
+    INSTALLATION_CHECK $? $SOFTWARE
+else
+    echo "Already Installed..No need to install again"
+
+fi
